4f04ee771092ff26a902bd4be05731f84fec9fe6
leonardo <lvcz@github.com>
2016-04-02 19:05:37 -0300

    alteração da função le_grafo

---
+++ trabalho-1/leonardo/trabalho-1/grafo.c
@@ -0,0 +1,311 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <graphviz/cgraph.h>
+#include "grafo.h"
+
+typedef struct aresta *aresta;
+
+//------------------------------------------------------------------------------
+// (apontador para) estrutura de dados para representar um grafo
+// 
+// o grafo pode ser
+// - direcionado ou não
+// - com pesos nas arestas ou não
+// 
+// além dos vértices e arestas, o grafo tem um nome, que é uma "string"
+// 
+// num grafo com pesos nas arestas todas as arestas tem peso, que é um long int
+// 
+// o peso default de uma aresta é 0
+
+struct grafo {
+
+	char        *nome;
+	lista		 vertices;
+	int          direcionado;
+	int          ponderado;
+	unsigned int n_vertices;
+	unsigned int n_arestas;
+};
+
+struct vertice {
+
+	char *nome;
+	lista vizinhos_saida;
+	lista vizinhos_entrada;
+};
+
+struct aresta {
+	
+	vertice  origem;
+	vertice  destino;
+	long int peso;
+};
+
+//------------------------------------------------------------------------------
+// devolve o nome do grafo g
+
+char *nome_grafo(grafo g) {
+
+	return g->nome;
+}
+
+//------------------------------------------------------------------------------
+// devolve 1, se g é direcionado, ou
+//         0, caso contrário
+
+int direcionado(grafo g) {
+
+	return g->direcionado;
+}
+
+//------------------------------------------------------------------------------
+// devolve 1, se g tem pesos nas arestas/arcos,
+//      ou 0, caso contrário
+
+int ponderado(grafo g) {
+
+	return g->ponderado;
+}
+
+//------------------------------------------------------------------------------
+// devolve o número de vértices do grafo g
+
+unsigned int n_vertices(grafo g) {
+
+	return g->n_vertices;
+}
+
+//------------------------------------------------------------------------------
+// devolve o número de arestas/arcos do grafo g
+
+unsigned int n_arestas(grafo g) {
+
+	return g->n_arestas;
+}
+
+//------------------------------------------------------------------------------
+// devolve o nome do vertice v
+
+char *nome_vertice(vertice v) {
+
+	return v->nome;
+}
+
+//------------------------------------------------------------------------------
+// lê um grafo no formato dot de input, usando as rotinas de libcgraph
+// 
+// desconsidera todos os atributos do grafo lido exceto o atributo
+// "peso" quando ocorrer; neste caso o valor do atributo é o peso da
+// aresta/arco que é um long int
+// 
+// num grafo com pesos todas as arestas/arcos tem peso
+// 
+// o peso default de uma aresta num grafo com pesos é 0
+// 
+// todas as estruturas de dados alocadas pela libcgraph são
+// desalocadas ao final da execução
+// 
+// devolve o grafo lido ou
+//         NULL em caso de erro 
+
+grafo le_grafo(FILE *input) {
+
+    Agraph_t *ag = agread(input, 0);
+    if(!(ag && agisstrict(ag))) 
+        return NULL;
+
+    grafo g = malloc(sizeof(struct grafo));
+    if( !g ) return NULL;
+
+    g->nome        = agnameof(ag);
+    g->direcionado = agisdirected(ag);
+    g->n_vertices  = (unsigned int)agnnodes(ag);
+    g->n_arestas   = (unsigned int)agnedges(ag);
+	g->vertices= constroi_lista();
+	
+	for(Agnode_t *v=agfstnode(ag); v; v=agnxtnode(ag,v)){
+		vertice vert= malloc(sizeof(struct vertice));
+		vert->nome= agnameof(v);
+		vert->vizinhos_saida= constroi_lista();
+		vert->vizinhos_entrada= constroi_lista();
+		insere_lista(vert,g->vertices);
+		if(!g->direcionado){
+			for (Agedge_t *a=agfstedge(g,v); a; a=agnxtedge(g,a,v)){
+				aresta edge= malloc (sizeof(struct aresta);
+				edge->origem= vert;
+				char *w = agget(a,(char*)"peso");
+                    edge->peso = w ? strtod(w,NULL) : 0.0;
+				//edge->destino= não sei oq por aqui ainda
+				insere_lista(edge,vert->vizinhos_entrada);
+				insere_lista(edge, vert->vizinhos_saida);
+				
+			}
+		}
+	
+	
+	}
+	
+
+
+	if (!input)
+		 return NULL;
+	else return g;
+}  
+
+//------------------------------------------------------------------------------
+// desaloca toda a memória usada em *g
+// 
+// devolve 1 em caso de sucesso ou
+//         0 caso contrário
+// 
+// g é um (void *) para que destroi_grafo() possa ser usada como argumento de
+// destroi_lista()
+
+int destroi_grafo(void *g) {
+
+	if (g) 
+		 return 1;
+	else return 0;
+
+}
+
+//------------------------------------------------------------------------------
+// escreve o grafo g em output usando o formato dot, de forma que
+// 
+// 1. todos os vértices são escritos antes de todas as arestas/arcos 
+// 
+// 2. se uma aresta tem peso, este deve ser escrito como um atributo 
+//    de nome "peso"
+//
+// devolve o grafo escrito ou
+//         NULL em caso de erro 
+
+grafo escreve_grafo(FILE *output, grafo g) {
+	
+	
+//    int i,j;
+
+    if(!(g && output)) return NULL;
+/*    
+    printf("strict %sgraph \"%s\" {\n\n", g->direcionado ? "di" : "", g->nome);
+    
+    // Imprime vertices
+    for(i=0; i < g->n_vertices; i++){
+        printf("    \"%s\"\n", g->l_vert[i]);
+    }
+    printf("\n");
+
+
+    // Imprime arestas
+    const char *dir = g->eh_dir ? "->" : "--";
+    for(j=0; j < g->n_ares; j++){
+        printf("    \"%s\" %s \"%s\"", g->l_tail[j], dir, g->l_head[j]);
+        if(g->l_peso[j] > 0.0) printf(" [peso=%f]", g->l_peso[j]);
+        printf("\n");
+    }
+
+    printf("}\n");
+*/
+    return g;
+}
+
+//------------------------------------------------------------------------------
+// devolve um grafo igual a g
+
+grafo copia_grafo(grafo g) {
+	
+	if( g ) 
+		 return g;
+	else return NULL;
+
+}
+
+//------------------------------------------------------------------------------
+// devolve a vizinhança do vértice v no grafo g
+// 
+// se direcao ==  0, v é um vértice de um grafo não direcionado
+//                   e a função devolve sua vizinhanca 
+//
+// se direcao == -1, v é um vértice de um grafo direcionado e a função
+//                   devolve sua vizinhanca de entrada
+//
+// se direcao ==  1, v é um vértice de um grafo direcionado e a função
+//                   devolve sua vizinhanca de saída
+
+lista vizinhanca(vertice v, int direcao, grafo g) {
+
+	lista l = NULL;
+	
+	if ( g && v && direcao )
+		 return NULL;
+	else return l;
+
+}
+
+//------------------------------------------------------------------------------
+// devolve o grau do vértice v no grafo g
+// 
+// se direcao == 0, v é um vértice de um grafo não direcionado
+//                  e a função devolve seu grau
+//
+// se direcao == -1, v é um vértice de um grafo direcionado
+//                   e a função devolve seu grau de entrada
+//
+// se direcao == 1, v é um vértice de um grafo direcionado
+//                  e a função devolve seu grau de saída
+
+unsigned int grau(vertice v, int direcao, grafo g) {
+	
+	if ( g && v && direcao )
+		 return 1;
+	else return 0;
+}
+
+//------------------------------------------------------------------------------
+// devolve 1, se o conjunto dos vertices em l é uma clique em g, ou
+//         0, caso contrário
+//
+// um conjunto C de vértices de um grafo é uma clique em g 
+// se todo vértice em C é vizinho de todos os outros vértices de C em g
+
+int clique(lista l, grafo g) {
+	
+	if( g && l )
+		 return 1;
+	else return 0;
+
+}
+
+
+//------------------------------------------------------------------------------
+// devolve 1, se v é um vértice simplicial em g, ou
+//         0, caso contrário
+//
+// um vértice é simplicial no grafo se sua vizinhança é uma clique
+
+int simplicial(vertice v, grafo g) {
+	
+	if( g && v ) 
+		 return 1;
+	else return 0;
+}
+
+//------------------------------------------------------------------------------
+// devolve 1, se g é um grafo cordal ou
+//         0, caso contrário
+//
+// um grafo (não direcionado) G é cordal 
+// se e somente se 
+// existe uma permutação 
+//     (v_1, ..., v_n) de V(G)
+// tal que
+//     v_i é simplicial em G - v_1 - ... - v_{i-1}
+
+int cordal(grafo g) {
+
+	if( g )
+		 return 1;
+	else return 0;
+}
+
