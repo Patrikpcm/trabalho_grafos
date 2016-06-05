80d551ff5e22a118dd92e27114aa2bf0bae4fef3
alexandre <alexandre@localhost.localdomain>
2016-06-05 18:12:49 -0300

    Pasta do T2 para testes

---
+++ trabalho-2/T2Teste/teste.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lista.h"
+#include "grafo.h"
+
+//------------------------------------------------------------------------------
+
+int main(void) {
+
+ grafo g = le_grafo(stdin);
+
+  if ( !g ) return 1;
+
+  printf("nome: %s\n", nome_grafo(g));
+  int d = direcionado(g);
+  printf("%sdirecionado\n", d ? "" : "não ");
+  printf("%sponderado\n", ponderado(g) ? "" : "não ");
+  unsigned n = n_vertices(g);
+  printf("%d vértices\n", n);
+  printf("%d arestas\n", n_arestas(g));
+  escreve_grafo(stdout, g);
+  if (!d)  printf("\n%s %s é cordal\n", nome_grafo(g), cordal(g) ? "" : "não ");
+
+
+  return ! destroi_grafo(g);
+}
